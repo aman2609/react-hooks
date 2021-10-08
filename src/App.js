e7ebac92a64213a541ba638d4257c4fb5abe01be
Aman Singh <aman26092000@gmail.com>
2021-10-08 21:10:59 +0530

    useEffect case 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState ,useEffect } from "react";
 
 let App=()=> {
   let [va,setVa]=useState(0);
+  console.log("app");
+  // useEffect is a hook which is used only inside functional component 
+  // it takes 2 input =>function & array(optional)
+  // based on array passed or not we have 3 cases
+  // case 1
+  // Empty array is passed
+  // useEffect is caleed only once in this case only after first render(like componentDidMount)
+  useEffect(()=>{
+    console.log("useEffect was called")
+  },[])
   return (
+    
     <div>
+      {console.log("ret")}
       <button onClick={()=>{
         setVa(va+1);
       }}>+</button>
