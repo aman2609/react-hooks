c4f3fc0f85f68e4de32c6ed3f8ae801e7b5d95a5
Aman Singh <aman26092000@gmail.com>
2021-10-09 01:02:00 +0530

    Clean Up case 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState ,useEffect } from "react";
 
 let App=()=> {
   let [va,setVa]=useState(0);
-  let [process,setProcess]=useState("running");
+  // let [process,setProcess]=useState("running");
   console.log("app");
   // useEffect is a hook which is used only inside functional component 
   // it takes 2 input =>function & array(optional)
@@ -18,17 +18,20 @@ let App=()=> {
   // case 2
   // no array is passed
   // useEffect is caleed after render & each rerender
-  // useEffect(()=>{
-  //   console.log("useEffect case 2 was called");
-  // })
+  useEffect(()=>{
+    console.log("useEffect case 2 was called");
+    return ()=>{
+      console.log("cleanup function");
+    }
+  })
 
   // case 3
   // array is passed with one of state variable
   // runs on first render & for change in state variable provided in array
-  useEffect(()=>{
-    console.log("useEffect case 3")
-    console.log(process);
-  },[process])
+  // useEffect(()=>{
+  //   console.log("useEffect case 3")
+  //   console.log(process);
+  // },[process])
 
 
   return (
@@ -42,7 +45,7 @@ let App=()=> {
       <button onClick={()=>{
         setVa(va-1);
       }}>-</button>
-      <div>
+      {/* <div>
         <h2>{process}</h2>
       </div>
       <button onClick={()=>{
@@ -52,7 +55,7 @@ let App=()=> {
           setProcess("running");
         }
         
-      }}>Kill Process</button>
+      }}>Kill Process</button> */}
     </div>
   );
 }
